9b561155726f922db521c87092edad19c72b7287
Jun Zhao <93747539+JunZhao20@users.noreply.github.com>
2023-10-29 20:24:27 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-
+// Once the paste event is triggered it will stop any further propagation of the event
+const pasteEnabler = function(e) {
+  e.stopImmediatePropagation()
+}
+// Detects paste event to trigger pasteEnabler which will allow you to paste
+document.addEventListener(
+  //waiting for paste event
+  "paste",
+  pasteEnabler,
+  // captures paste event at the start
+  true
+  );
+  
